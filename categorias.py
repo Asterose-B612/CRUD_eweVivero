fb29a1747c7410663acd74d25dbe539824f7da8e
KarinaR <decodikiaro@gmail.com>
2023-07-19 04:41:50 -0300

    mi primer cud

---
+++ categorias.py
@@ -0,0 +1,159 @@
+"""  
+Árboles
+Plantas perennes y de gran tamaño que tienen un tronco leñoso y se ramifican en la parte superior. Pueden ser de hoja caduca o perenne.
+
+Arbustos 
+Plantas más pequeñas que los árboles, con múltiples tallos que se ramifican desde la base.
+
+Hierbas
+ Plantas generalmente de tallos suaves y flexibles, y no desarrollan una estructura leñosa. Pueden ser anuales, bianuales o perennes.
+
+Plantas con flores
+Amplia variedad de plantas, pueden incluir árboles, arbustos, hierbas, plantas trepadoras y plantas de interior.
+
+.....................................
+CATEGORIAS PLANTAS: PL
+CATEGORIAS ARBOLES : ARBO
+
+las ultimas letras corresponden a las dos primeras letras del nombre del producto +el numero
+
+1)
+nombre: Roble (Quercus robur)
+ID:1
+Codigo: PLAARRO1
+Descripción: Un árbol de hoja caduca con un tronco fuerte y majestuoso. Crece hasta alcanzar alturas impresionantes y produce bellotas.
+Precio:30000
+categoria: ARBOLES
+
+2)
+nombre: Pino (Pinus spp.)
+ID:2
+Codigo: PLAARBOPI1
+Descripción: Un árbol de hoja perenne con forma de cono y hojas en forma de aguja. Es conocido por su madera y su capacidad de adaptación a diferentes climas.
+Precio: 25000
+categoria: ARBOLES
+
+3)
+nombre: Manzano (Malus domestica)
+ID:3
+Codigo: PLAARBOMA1
+Descripción: El manzano es un árbol frutal de hoja caduca que produce manzanas. Es originario de Asia Central y se cultiva ampliamente en todo el mundo por sus frutos comestibles y su belleza ornamental. Hay muchas variedades de manzanos, que varían en tamaño, forma y sabor de las manzanas.
+Precio: 35000
+categoria: ARBOLES
+
+4)
+nombre:  Sauce (Salix spp.)
+ID:4
+Codigo: PLAARBOSA1
+Descripción: El sauce es un género de árboles y arbustos caducifolios que pertenecen a la familia Salicaceae. Hay muchas especies de sauce, y se caracterizan por sus ramas colgantes y flexibles. Algunas variedades populares incluyen el sauce llorón (Salix babylonica) y el sauce blanco (Salix alba). Los sauces son apreciados por su elegancia y se cultivan en jardines y parques.
+Precio: 60000
+categoria: ARBOLES
+...............................................
+CATEGORIAS: ARBUSTOS  ARBU
+
+1)
+nombre: Azalea (Rhododendron spp.):
+ID:1
+Codigo: PLAARBUAZ1
+Descripción: Un arbusto de hoja perenne que produce hermosas flores en primavera. Hay muchas variedades con diferentes colores de flores.
+Precio: 10000
+categoria: ARBUSTOS
+
+2)
+nombre: Boj (Buxus sempervirens)
+ID:2
+Codigo: PLAARBUBO1
+Descripción: Un arbusto de hoja perenne con hojas pequeñas y densas. Se utiliza comúnmente para setos y topiarios debido a su capacidad de poda.
+Precio: 120000
+categoria: ARBUSTOS
+
+3)
+nombre: Lavanda (Lavandula spp.)
+ID:3
+Codigo: PLAARBULA1
+Descripción: La lavanda es una planta perenne con flores fragantes y hojas estrechas y lanceoladas. Es conocida por su aroma relajante y sus propiedades terapéuticas. La lavanda se cultiva ampliamente en jardines y se utiliza en la industria cosmética y de aromaterapia.
+Precio: 15000
+categoria: ARBUSTOS
+
+4)
+nombre: Adelfa (Nerium oleander)
+ID:4
+Codigo: PLAARBUAD1
+Descripción: La adelfa es un arbusto perenne de hoja perenne con flores grandes y vistosas en tonos de rosa, blanco y rojo. Es apreciada por su belleza y se utiliza a menudo como planta ornamental en jardines y paisajes. Sin embargo, es importante tener en cuenta que todas las partes de la adelfa son altamente tóxicas si se ingieren.
+Precio: 120000
+categoria: ARBUSTOS
+
+.....................................
+CATEGORIAS HIERBAS :  HIER
+
+1)
+nombre: Cilantro (Coriandrum sativum):
+ID:1
+Codigo: PLAHIERCI1
+Descripción: Una hierba anual con hojas verdes y aromáticas. Se utiliza tanto las hojas como las semillas en la cocina para añadir sabor.
+Precio: 4000
+categoria: HIERBAS
+
+2)
+nombre: Menta (Mentha spp.):
+ID:2
+Codigo: PLAHIERME1
+Descripción: Una hierba perenne que se extiende rápidamente a través de rizomas. Tiene un aroma refrescante y se utiliza en infusiones y cócteles.
+Precio: 4000
+categoria: HIERBAS
+
+3)
+nombre: Romero (Rosmarinus officinalis)
+ID:3
+Codigo: PLAHIERRO1
+Descripción: El romero es una planta perenne aromática con hojas pequeñas y puntiagudas. Tiene un aroma distintivo y se utiliza comúnmente como hierba culinaria y en productos de cuidado personal. Es nativo de la región mediterránea y es apreciado por su sabor y propiedades medicinales.
+Precio: 4000
+categoria: HIERBAS
+
+4)
+nombre: Nombre: Perejil (Petroselinum crispum)
+ID:4
+Codigo: PLAHIERPE1
+Descripción: El romero es una planta perenne aromática con hojas pequeñas y puntiagudas. Tiene un aroma distintivo y se utiliza comúnmente como hierba culinaria y en productos de cuidado personal. Es nativo de la región mediterránea y es apreciado por su sabor y propiedades medicinales.
+Precio: 4000
+categoria: HIERBAS
+
+...................................
+CATEGORIAS: PLANTAS CON FLORES:  CFLOR
+
+1)
+nombre: Rosa (Rosa spp.)
+ID:1
+Codigo: PLACFLORRO1
+Descripción: Una planta con flores que es conocida por su belleza y fragancia. Hay muchas variedades y colores disponibles. Las rosas se utilizan comúnmente en jardines y arreglos florales.
+Precio: 55000
+categoria: PLANTAS CON FLORES
+
+2)
+nombre:  Orquídea (Orchidaceae)
+ID:2
+Codigo: PLACFLOROR1
+Descripción: Una planta con flores exóticas y elegantes. Las orquídeas son apreciadas por su belleza y diversidad de colores y formas. Son populares como plantas de interior.
+Precio: 65000
+categoria: PLANTAS CON FLORES
+
+3)
+Nombre: Girasol (Helianthus annuus)
+ID:3
+Codigo: PLACFLORGI1
+Descripción: Los girasoles son plantas anuales conocidas por sus grandes flores amarillas brillantes y su característica forma de seguir el movimiento del sol a lo largo del día. Son nativos de América del Norte y son apreciados por su belleza y por ser símbolos de alegría y energía positiva.
+Precio: 50000
+categoria: PLANTAS CON FLORES
+
+
+4)
+Nombre: Tulipán (Tulipa spp.)
+ID:4
+Codigo: PLACFLORTU1
+Descripción: Los tulipanes son plantas bulbosas perennes conocidas por sus flores grandes y vibrantes en una amplia gama de colores, como rojo, rosa, amarillo, blanco y púrpura. Son originarios de Europa y Asia y son ampliamente cultivados como plantas ornamentales en jardines y parques.
+Precio: 80000
+categoria: PLANTAS CON FLORES
+
+
+
+"""
